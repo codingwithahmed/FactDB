15ebe7bb52456e02d7ef951fd55d0fd68914bcca
codingwithahmed <aziahmedfarooq@gmail.com>
2021-05-18 01:44:09 +0500

    FactDb.net footer

--- front/src/components/Footer.js
+++ front/src/components/Footer.js
@@ -12,7 +12,7 @@ export default function Footer() {
             <div className="footer_links">
              <Link to="/help" className="footer_link"><p className="footer_link">Contact</p></Link>
              <Link to="/Legal" className="footer_link"> <p className="footer_link">Legal</p></Link>
-             <p className="footer_link">©copyright 2021 <a href="https://www.factdb.net">factdb.net</a></p>
+             <p className="footer_link">©copyright 2021 <a href="https://factdb.net">factdb.net</a></p>
              <Link to="/business" className="footer_link"> <p className="footer_link">Business</p></Link>
              <Link to="/trysearch" className="footer_link">  <p className="footer_link">Search </p></Link>
             </div>
